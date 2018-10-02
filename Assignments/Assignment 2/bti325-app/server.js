bdb9406d8173ad9d89056b43c54b52a153453959
Renniej <tai.juan2@gmail.com>
2018-10-01 22:53:04 -0400

    Finished Part 1

--- Assignments/Assignment 2/bti325-app/server.js
+++ Assignments/Assignment 2/bti325-app/server.js
@@ -4,14 +4,23 @@ var app = express(); //express application
 var path = require("path"); //path module
 
 var HTTP_PORT = process.env.PORT ||8080;
-
+app.use(express.static('public')); 
 
 function On_HTTP_Start(){
     console.log("Express http server listening on " + HTTP_PORT);
 }
 
+app.get('/about', function(req, res) {
+    res.sendFile("/views/about.html", {root: __dirname});
+});
 
+app.get('/', function(req, res){
+
+    res.sendFile("/views/home.html", {root : __dirname});
+
+});
 
 
 app.listen(HTTP_PORT, On_HTTP_Start);
+
 
